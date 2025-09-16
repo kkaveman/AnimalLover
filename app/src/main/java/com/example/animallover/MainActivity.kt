671d26ced02d933faa138d3f17d801a410251710
zzzlkg <amranthaq1fr@gmail.com>
2025-09-16 23:13:50 +0700

    UI Progress#1

--- app/src/main/java/com/example/animallover/MainActivity.kt
+++ app/src/main/java/com/example/animallover/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.animallover
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
         }
+
+        // Handle bottom navigation item clicks
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_popular -> {
+                    // For now, just show home fragment
+                    // You can create other fragments later
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_add -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_chat -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_profile -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
